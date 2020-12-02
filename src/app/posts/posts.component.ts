79cf80f7e4e2a38c074f468405b71a5c79709c6f
meTheLearner <togunchan@gmail.com>
2020-12-03 01:35:23 +0300

    1-Http Get Request

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {
+  posts;
+  constructor(private http: HttpClient) {
+
+    
+
+    http.get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe(response => {
+      this.posts = response;
+    });
+
+
+   }
+
+
+}
