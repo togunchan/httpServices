a9513daa59a0703c505305d0d796c029099bdefa
meTheLearner <togunchan@gmail.com>
2020-12-05 01:33:41 +0300

    3-Http Put ve Patch Request

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,23 +11,36 @@ export class PostsComponent {
   private url = "https://jsonplaceholder.typicode.com/posts"
   constructor(private http: HttpClient) {
     http.get(this.url)
-    .subscribe(response => {
-      this.posts = <any>response;
-    });
+      .subscribe(response => {
+        this.posts = <any>response;
+      });
 
 
-   }
+  }
 
-   createPost(input: HTMLInputElement){
-    const post = {title: input.value};
-    input.value='';
-    this.http.post(this.url,JSON.stringify(post))
-        .subscribe(response => {
-          post['id']=response['id'];
-          this.posts.splice(0, 0, post);
-          
-        })
-   }
+  createPost(input: HTMLInputElement) {
+    const post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
+
+      })
+  }
+
+  updatePost(post) {
+    post.title = "updated";
+
+    this.http.put(this.url+"/"+post.id,JSON.stringify(post)).subscribe(response => console.log(response));
+    // this.http.patch(this.url + "/" + post.id, JSON.stringify({
+    //   title: "updated"
+    // })).subscribe(response => {
+    //   console.log(response);
+    // });
+
+
+  }
 
 
 }
